e9dcdfbbf7da1fb474b0ccfc968e3ad0f063e2a9
LostPigMessenger <ivelinihristov@gmail.com>
2021-03-30 04:09:16 +0100

    First steps

--- src/app/search-result/search-result.page.ts
+++ src/app/search-result/search-result.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonService } from '../services/pokemon.service';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-search-result',
@@ -10,15 +11,22 @@ import { PokemonService } from '../services/pokemon.service';
 export class SearchResultPage implements OnInit {
 
   details: any;
+  
+  indexes: any;
 
-  constructor(private route: ActivatedRoute, private pokeService: PokemonService) { }
+  constructor(private route: ActivatedRoute, private pokeService: PokemonService, private storage: Storage) { }
 
   ngOnInit() {
+
+    
+
     let index = this.route.snapshot.paramMap.get('index');//This gives us the pokemon that was chosen from the search result based on its index
     this.pokeService.getPokeDetails(index).subscribe(details => {
       console.log('Details: ', details);
       this.details = details;
     });
+
+    
   }
 
 }
